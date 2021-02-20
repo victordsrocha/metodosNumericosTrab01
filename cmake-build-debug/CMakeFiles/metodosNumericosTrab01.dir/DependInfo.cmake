cdef5fee918c494c53f18ec65c9480602d1631e3
Victor Rocha <victordsrocha@gmail.com>
2021-02-20 15:23:16 -0300

    Configurações iniciais

---
+++ cmake-build-debug/CMakeFiles/metodosNumericosTrab01.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/victor/CLionProjects/metodosNumericosTrab01/main.cpp" "/home/victor/CLionProjects/metodosNumericosTrab01/cmake-build-debug/CMakeFiles/metodosNumericosTrab01.dir/main.cpp.o"
+  "/home/victor/CLionProjects/metodosNumericosTrab01/sdfsd.cpp" "/home/victor/CLionProjects/metodosNumericosTrab01/cmake-build-debug/CMakeFiles/metodosNumericosTrab01.dir/sdfsd.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/metodosNumericosTrab01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/metodosNumericosTrab01.dir/main.cpp.o"
+  "CMakeFiles/metodosNumericosTrab01.dir/sdfsd.cpp.o"
+  "metodosNumericosTrab01"
+  "metodosNumericosTrab01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/metodosNumericosTrab01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
